b054e15a047b09da24278e87d9ba54a4c34da011
Tony <anthonysheer99@hotmail.com>
2021-10-28 19:40:40 +1000

    lower order of complexity (o3 -> o2)

--- python_code/three_sum.py
+++ python_code/three_sum.py
@@ -1,8 +1,6 @@
 
-from typing import List
-
-
 from collections import defaultdict
+from typing import List, Set
 
 
 def three_sum(nums: List[int]) -> List[List[int]]:
@@ -18,13 +16,27 @@ def three_sum(nums: List[int]) -> List[List[int]]:
 
     # A basic strategy, for every pair check for the third unique number to make them add to 0
     triples = set()
-    for i, first_num in enumerate(nums):
-        for j, second_num in enumerate(nums[i + 1:]):
-            j = j + i + 1
-            required_number = 0 - first_num - second_num
-            if required_number in nums[j + 1:]:  # Only look for required num
+    for first_num, indexes in numbers_to_indexes.items():
+        # Check for triples, i.e. three zeros
+        if 3 * first_num == 0 and len(indexes) > 2:
+            triples.add(
+                tuple(sorted((first_num, first_num, first_num))))
 
+        # Check for solutions involving this number as a double
+        if len(indexes) > 1:
+            required_number = 0 - first_num - first_num
+            if required_number in numbers_to_indexes and required_number != first_num:
                 triples.add(
-                    tuple(sorted((first_num, second_num, required_number))))
+                    tuple(sorted((first_num, first_num, required_number))))
+
+        # Go through pairs
+        for second_num in numbers_to_indexes.keys():
+            if first_num != second_num:  # already handled case of 2 or 3 copies of num
+                required_number = 0 - first_num - second_num
+
+                # already handled 2x num case so will exclude here
+                if required_number in numbers_to_indexes and required_number not in (first_num, second_num):
+                    triples.add(
+                        tuple(sorted((first_num, second_num, required_number))))
 
     return [list(triple) for triple in triples]
