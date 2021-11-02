a5957fc816ad409b7cffb9341a0f05f7ecd0b4d7
Tony <anthonysheer99@hotmail.com>
2021-11-02 18:06:38 +1000

    three_sum_closest_search

--- python_code/three_sum_closest.py
+++ python_code/three_sum_closest.py
@@ -1,6 +1,89 @@
 from typing import List
 
 
+class Node(object):
+    def __init__(self, indexes: List[int]) -> None:
+        self.indexes = tuple(sorted(indexes))
+        self._total = None
+
+    def total(self, numbers: List[int]) -> int:
+        return sum(numbers[i] for i in self.indexes)
+
+    def neighbouring_nodes(self, tick: int, max_index: int, searched: set, near_solution: bool):
+        nodes = []
+        i, j, k = self.indexes
+        if 0 <= i + tick < max_index and i + tick != j and i + tick != k:
+            node = Node([i + tick, j, k])
+            if node not in searched:
+                nodes.append(node)
+        if 0 <= j + tick < max_index and i != j + tick and j + tick != k:
+            node = Node([i, j + tick, k])
+            if node not in searched:
+                nodes.append(node)
+        if 0 <= k + tick < max_index and i != k + tick and j != k + tick:
+            node = Node([i, j, k + tick])
+            if node not in searched:
+                nodes.append(node)
+
+        if not near_solution:
+            return nodes[:1]
+        return nodes
+
+    def __eq__(self, o: object) -> bool:
+        return self.indexes == o.indexes
+
+    def __hash__(self) -> int:
+        return self.indexes.__hash__()
+
+    def __str__(self) -> str:
+        return self.indexes.__str__()
+
+    def __repr__(self) -> str:
+        return self.indexes.__repr__()
+
+
+def three_sum_closest_search(nums: List[int], target: int) -> int:
+    nums = sorted(nums)
+    max_index = len(nums)
+    current = Node([0, 1, 2])
+    searched = {current}
+    near_solution = False
+    to_search = set(current.neighbouring_nodes(
+        1, max_index, searched, near_solution))
+
+    closest_sum = current.total(nums)
+    closest_distance = abs(closest_sum - target)
+    # need to use greedy heuristic to get 'near' solution (distance changes from - to +)
+
+    # Search method below will search one step up to increase total, or one step down to decrease
+    while to_search:
+        current = to_search.pop()
+        searched.add(current)
+
+        current_sum = current.total(nums)
+
+        if not near_solution and current_sum > target:
+            near_solution = True
+            searched = set()
+
+        if current_sum == target:
+            return current_sum
+        elif current_sum < target:
+            to_search.update(current.neighbouring_nodes(
+                1, max_index, searched, near_solution))
+            # search up
+        elif current_sum > target:
+            # search down
+            to_search.update(current.neighbouring_nodes(
+                -1, max_index, searched, near_solution))
+
+        current_distance = abs(current_sum - target)
+        if current_distance < closest_distance:
+            closest_distance, closest_sum = current_distance, current_sum
+
+    return closest_sum
+
+
 def three_sum_closest(nums: List[int], target: int) -> int:
     """
     Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
